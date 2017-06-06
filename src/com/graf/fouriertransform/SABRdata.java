a05fd5eb46f9499b0d5b7d5cdf30a21741637873
AndyGraf <andy_xxl@hotmail.de>
2017-06-06 09:14:50 +0200

    data extraction files

--- src/com/graf/fouriertransform/SABRdata.java
+++ src/com/graf/fouriertransform/SABRdata.java
@@ -32,5 +32,59 @@ public class SABRdata {
 		
 		return data;
 	}
+	
+	public double[][] getForwardCurveData() throws IOException {
+		int rows = 97;
+		int columns = 5;
+		double[][] data = new double[rows][columns];
+		for(int i=0; i< rows;i++){
+			for(int j=0;j<columns; j++){
+				data[i][j] = Double.parseDouble(volatilitiesAndCurves.getValueAt(j+1, i+27).toString());
+			}
+		}
+		
+		return data;
+	}
+	
+	public double[][] getDiscountCurveData() throws IOException {
+		int rows = 107;
+		int columns = 5;
+		double[][] data = new double[rows][columns];
+		for(int i=0; i< rows;i++){
+			for(int j=0;j<columns; j++){
+				data[i][j] = Double.parseDouble(volatilitiesAndCurves.getValueAt(j+8, i+27).toString());
+			}
+		}
+		
+		return data;
+	}
+	
+	public double[][] getSmileData(int tenor) throws IOException {
+		int rows = 6;
+		int columns = 9;
+		double[][] data = new double[rows][columns];
+		if(tenor == 2){
+			for(int i=0; i< rows;i++){
+				for(int j=0;j<columns; j++){
+					data[i][j] = Double.parseDouble(smile.getValueAt(j+1, i+2).toString());
+				}
+			}
+		}
+		if(tenor == 5){
+			for(int i=0; i< rows;i++){
+				for(int j=0;j<columns; j++){
+					data[i][j] = Double.parseDouble(smile.getValueAt(j+1, i+11).toString());
+				}
+			}
+		}
+		if(tenor == 10){
+			for(int i=0; i< rows;i++){
+				for(int j=0;j<columns; j++){
+					data[i][j] = Double.parseDouble(smile.getValueAt(j+1, i+20).toString());
+				}
+			}
+		}
+		return data;
+	}
 
 }
--- src/com/graf/fouriertransform/dataTest.java
+++ src/com/graf/fouriertransform/dataTest.java
@@ -10,11 +10,22 @@ public class dataTest {
 
 	public static void main(String[] args) throws IOException {
 		SABRdata sheetdata = new SABRdata();
-		double[][] data = new double[10][14];
-		data = sheetdata.getVolatilities();
 		
-		int rows = 10;
-		int columns = 14;
+//		double[][] data = new double[10][14];
+//		data = sheetdata.getVolatilities();
+//		int rows = 10;
+//		int columns = 14;
+//		for(int i=0; i< rows;i++){
+//			for(int j=0;j<columns; j++){
+//				System.out.print(data[i][j] + "\t");
+//			}
+//			System.out.println();
+//		}
+		
+		int rows = 6;
+		int columns = 9;
+		double[][] data = new double[rows][columns];
+		data = sheetdata.getSmileData(2);
 		for(int i=0; i< rows;i++){
 			for(int j=0;j<columns; j++){
 				System.out.print(data[i][j] + "\t");
@@ -24,6 +35,16 @@ public class dataTest {
 		
 		
 		
+		
+		
+		
+//		
+//		Sheet volatilitiesAndCurves;
+//		File file = new File("src/Data.ods");
+//		volatilitiesAndCurves = SpreadSheet.createFromFile(file).getSheet(0);
+//		
+//		System.out.println(Double.parseDouble(volatilitiesAndCurves.getValueAt(5, 27).toString()));
+//		
 	}
 
 }
